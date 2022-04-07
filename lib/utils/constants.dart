08f3ddb06cab6287a5a834d2d009792463c3c7c2
ranloock <ran.trisk@gmail.com>
2022-04-08 01:23:23 +0300

    fixed animation bug

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 const kJsonUrlRoot = 'http://jsonplaceholder.typicode.com/';
 const kInitialPostsPageRouteName = '/posts';
 const kTestAssignLink = 'https://algoretail.co.il/test/testAssignComment';
-const kCommentBarSize = 75.0;
+const kCommentBarSize = 125.0;
 const kAnimationMillisecondsTimer = 400;
